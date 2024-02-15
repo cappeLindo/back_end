fe3caf6f004f14dca1f3166a6a75c4db7c420164
Vitor Gabriel Capelleto <2023103030028@aluno.ifro.edu.br>
2024-02-15 11:20:26 -0400

    pirmeiro comiit

---
+++ variavel.js
@@ -0,0 +1,7 @@
+var nome = "notebook"
+var preco = 2000 ;
+console.log(typeof nome, typeof preco);
+
+let linguagens = ["php", "java", "c#"];
+linguagens [0]= "vai tomar no cu pretinho";
+console.log(linguagens.length)
